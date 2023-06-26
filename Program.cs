cec4f8cdd6e96e1ba81aa7f004bf81e163d97a55
malmeidaw <matheus.997willians@outlook.com>
2023-06-26 16:26:39 -0300

    Changing the loop logic so that a new email is sent only if there is a change in price.

--- Program.cs
+++ Program.cs
@@ -32,43 +32,49 @@
                     return;
                 }
             }
-            
+            double lastPrice = -1;
 
             while (true)
             {
                 APIRequest a = new APIRequest();
                 double price = a.LastPrice(Symbol).Result;
                 if (price == -1) break;
-                if (price >= SellingReference)
+                
+                if (lastPrice!=price)
                 {
-                    Mail SellingMail = new Mail();
-                    MailConfRead mailConfRead = new MailConfRead();
-                    MailConf mailConf = new MailConf();
-                    mailConf = mailConfRead.MailConfFromFile();
-                    if (mailConf.ToAddress is null) break;
+                    if (price >= SellingReference)
+                    {
+                        Mail SellingMail = new Mail();
+                        MailConfRead mailConfRead = new MailConfRead();
+                        MailConf mailConf = new MailConf();
+                        mailConf = mailConfRead.MailConfFromFile();
+                        if (mailConf.ToAddress is null) break;
 
-                    string Recommendation = String.Format("Sell {0}", Symbol);
-                    string EmailBody = String.Format("We recommend selling your position in {0}, as its current price of {1} has exceeded the value of {2}.", Symbol, price, SellingReference);
+                        string Recommendation = String.Format("Sell {0}", Symbol);
+                        string EmailBody = String.Format("We recommend selling your position in {0}, as its current price of {1} has exceeded the value of {2}.", Symbol, price, SellingReference);
 
-                    SellingMail.sendMail(mailConf,Recommendation,EmailBody);
-                }
-                if (price <= BuyingReference)
-                {
-                    Mail SellingMail = new Mail();
-                    MailConfRead mailConfRead = new MailConfRead();
-                    MailConf mailConf = new MailConf();
-                    mailConf = mailConfRead.MailConfFromFile();
-                    if (mailConf.ToAddress is null) break;
+                        SellingMail.sendMail(mailConf, Recommendation, EmailBody);
+                    }
+                    if (price <= BuyingReference)
+                    {
+                        Mail SellingMail = new Mail();
+                        MailConfRead mailConfRead = new MailConfRead();
+                        MailConf mailConf = new MailConf();
+                        mailConf = mailConfRead.MailConfFromFile();
+                        if (mailConf.ToAddress is null) break;
 
-                    string Recommendation = String.Format("Buy {0}", Symbol);
-                    string EmailBody = String.Format("We recommend buying a position in {0}, as its current price of {1} is lower than {2}.", Symbol, price, BuyingReference);
+                        string Recommendation = String.Format("Buy {0}", Symbol);
+                        string EmailBody = String.Format("We recommend buying a position in {0}, as its current price of {1} is lower than {2}.", Symbol, price, BuyingReference);
 
-                    SellingMail.sendMail(mailConf, Recommendation, EmailBody);
+                        SellingMail.sendMail(mailConf, Recommendation, EmailBody);
+                    }
                 }
                 Console.WriteLine($"Monitoring {Symbol}. Current price is {price}");
+                lastPrice = price;
                 //Console.WriteLine(price);
                 //Thread.Sleep(60000);
                 Thread.Sleep(10000);
+                
             }
             
             Console.ReadKey();
